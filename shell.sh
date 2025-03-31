e591839eba97d501874840f98c383a309e834c4e
Vincent Ge <vincent@trunk.io>
2025-03-31 14:17:35 -0400

    test in ci

--- shell.sh
+++ shell.sh
@@ -3,13 +3,13 @@
 # A generic shell script that doesn't do anything useful
 
 # Print a welcome message
-echo "Welcome to a generic shell script!"
+echo 'Welcome to a generic shell script!'
 echo "This script doesn't do anything particularly useful."
 
 # Define some variables
-CURRENT_DATE=$(date)
+CURRENT_DATE=`date`
 RANDOM_NUMBER=$((RANDOM % 100))
-USER_NAME=$(whoami)
+USER_NAME=`whoami`
 
 # Display some information
 echo "-------------------------------------"
@@ -19,19 +19,33 @@ echo "Current user: $USER_NAME"
 echo "-------------------------------------"
 
 # Create a dummy loop
-for i in {1..5}; do
+for i in {1..5}
+do
     echo "Loop iteration $i"
     sleep 1
 done
 
 # Define a function that does nothing important
-function do_nothing() {
-    echo "This function doesn't do anything important"
+function do_nothing {
+    local message="This function doesn't do anything important"
+    echo $message
     echo "Just printing some text..."
+    return
 }
 
 # Call the function
 do_nothing
+
+# Check if a file exists (introducing an issue)
+if [ -f "/tmp/nonexistent_file" ]
+then
+    cat /tmp/nonexistent_file
+fi
+
+# Using ls in a loop (another issue)
+for file in $(ls /tmp); do
+    echo $file
+done
 
 # Exit with success
 echo "Script completed successfully!"
